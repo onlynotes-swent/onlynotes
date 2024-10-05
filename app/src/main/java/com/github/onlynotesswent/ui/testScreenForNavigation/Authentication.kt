d16f15c613389b5716fc0c1ea7aff4d2aa37a003
Lawrence Leitgib <lawrence11.leitgib@icloud.com>
2024-10-05 17:36:51 +0200

    feature: add navigation with NavHost to enable switching between screens

---
+++ app/src/main/java/com/github/onlynotesswent/ui/testScreenForNavigation/Authentication.kt
@@ -0,0 +1,23 @@
+package com.github.onlynotesswent.ui.testScreenForNavigation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.github.onlynotesswent.ui.navigation.NavigationActions
+import com.github.onlynotesswent.ui.navigation.Screen
+
+
+//simple empty screen for testing navigation
+@Composable
+fun AuthenticationScreen(navigationActions: NavigationActions) {
+    Column {
+
+
+    Text("Authentication Screen")
+    Button(onClick = { navigationActions.navigateTo(Screen.OVERVIEW) }) {
+        Text("Go to Overview")
+    }
+
+}
+}
