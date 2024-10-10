5947bb15397538342112c26de482f2fbcf2b76ea
Aisel Abibula <abibula.aisel@gmail.com>
2024-10-10 23:52:49 +0200

    test: improve unitTests for Scanner

--- app/src/test/java/com/github/onlynotesswent/model/scanner/ScannerTest.kt
+++ app/src/test/java/com/github/onlynotesswent/model/scanner/ScannerTest.kt
@@ -10,11 +10,13 @@ import com.github.onlynotesswent.MainActivity
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.gms.tasks.Task
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanner
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
 import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.any
 import org.mockito.kotlin.verify
@@ -23,70 +25,66 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class ScannerTest {
 
-  @Mock
-  private lateinit var mockMainActivity: MainActivity
-  @Mock
-  private lateinit var mockDocScanner: GmsDocumentScanner
-  @Mock
-  private lateinit var mockTaskIntentSender: Task<IntentSender>
-  @Mock
-  private lateinit var mockIntentSender: IntentSender
-  @Mock
-  private lateinit var mockActivityResultLauncher: ActivityResultLauncher<IntentSenderRequest>
+  @Mock private lateinit var mockMainActivity: MainActivity
+  @Mock private lateinit var mockDocScanner: GmsDocumentScanner
+  @Mock private lateinit var mockTaskIntentSender: Task<IntentSender>
+  @Mock private lateinit var mockIntentSender: IntentSender
+  @Mock private lateinit var mockActivityResultLauncher: ActivityResultLauncher<IntentSenderRequest>
 
   private lateinit var scanner: Scanner
 
   @Before
   fun setUp() {
-      MockitoAnnotations.openMocks(this)
+    MockitoAnnotations.openMocks(this)
 
-    Mockito.`when`(
-        mockMainActivity.registerForActivityResult(
-            any<ActivityResultContract<IntentSenderRequest, ActivityResult>>(),
-            any<ActivityResultCallback<ActivityResult>>()
-        )
-    )
+    `when`(
+            mockMainActivity.registerForActivityResult(
+                any<ActivityResultContract<IntentSenderRequest, ActivityResult>>(),
+                any<ActivityResultCallback<ActivityResult>>()))
         .thenReturn(mockActivityResultLauncher)
+
     scanner = Scanner(mockMainActivity, mockDocScanner)
   }
 
+  /** Test if scanner is correctly initialized, and if the activity result launcher is registered */
   @Test
   fun initTest() {
-    // Test if scanner is correctly initialized, and if the activity result launcher is registered
     scanner.init()
+
     verify(mockMainActivity)
         .registerForActivityResult(
             any<ActivityResultContract<IntentSenderRequest, ActivityResult>>(),
-            any<ActivityResultCallback<ActivityResult>>()
-        )
+            any<ActivityResultCallback<ActivityResult>>())
   }
 
   @Test
   fun scanSuccessTest() {
-    scanner.init()
+    val captor = ArgumentCaptor.forClass(IntentSenderRequest::class.java)
 
-    // Mock the creation of the IntentSender Task and simulate a successful result
-    Mockito.`when`(mockDocScanner.getStartScanIntent(mockMainActivity)).thenReturn(mockTaskIntentSender)
-    Mockito.`when`(mockTaskIntentSender.addOnSuccessListener(any())).thenAnswer {
-      (it.arguments[0] as OnSuccessListener<IntentSender>).onSuccess(mockIntentSender)
+    `when`(mockDocScanner.getStartScanIntent(mockMainActivity)).thenReturn(mockTaskIntentSender)
+    `when`(mockTaskIntentSender.addOnSuccessListener(any())).thenAnswer {
+      val listener = it.arguments[0] as OnSuccessListener<IntentSender>
+      listener.onSuccess(mockIntentSender)
       mockTaskIntentSender
     }
 
+    scanner.init()
     scanner.scan()
 
     verify(mockDocScanner).getStartScanIntent(mockMainActivity)
     verify(mockTaskIntentSender).addOnSuccessListener(any())
-    verify(mockActivityResultLauncher).launch(any())
+    verify(mockActivityResultLauncher).launch(captor.capture())
+    assertEquals(mockIntentSender, captor.value.intentSender)
   }
 
   @Test
   fun scanFailTest() {
+    `when`(mockDocScanner.getStartScanIntent(mockMainActivity)).thenReturn(mockTaskIntentSender)
+    `when`(mockTaskIntentSender.addOnSuccessListener(any())).thenReturn(mockTaskIntentSender)
+
     scanner.init()
-
-    Mockito.`when`(mockDocScanner.getStartScanIntent(mockMainActivity)).thenReturn(mockTaskIntentSender)
-    Mockito.`when`(mockTaskIntentSender.addOnSuccessListener(any())).thenReturn(mockTaskIntentSender)
-
     scanner.scan()
+
     verify(mockDocScanner).getStartScanIntent(mockMainActivity)
     verify(mockTaskIntentSender).addOnFailureListener(any())
   }
