184a465abe717d434d06b41229e0e7ad123ee62e
Roshan-y <115220107+Roshan-y@users.noreply.github.com>
2024-10-10 23:35:40 +0200

    feat: add UserCreate Screen to navigation
    added UserCreate Screen as a Screen in NavigationActions.kt and added it in NavHost in main

--- app/src/main/java/com/github/onlynotesswent/MainActivity.kt
+++ app/src/main/java/com/github/onlynotesswent/MainActivity.kt
@@ -7,16 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import com.github.onlynotesswent.model.users.UserViewModel
 import com.github.onlynotesswent.ui.navigation.NavigationActions
 import com.github.onlynotesswent.ui.navigation.Route
 import com.github.onlynotesswent.ui.navigation.Screen
 import com.github.onlynotesswent.ui.screen.AuthenticationScreen
 import com.github.onlynotesswent.ui.screen.OverviewScreen
 import com.github.onlynotesswent.ui.theme.SampleAppTheme
+import com.github.onlynotesswent.ui.user.UserCreate
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +32,7 @@ class MainActivity : ComponentActivity() {
 fun OnlyNotesApp() {
   val navController = rememberNavController()
   val navigationActions = NavigationActions(navController)
+  val userViewModel: UserViewModel = viewModel(factory = UserViewModel.Factory)
 
   NavHost(navController = navController, startDestination = Route.AUTH) {
     navigation(
@@ -36,6 +40,7 @@ fun OnlyNotesApp() {
         route = Route.AUTH,
     ) {
       composable(Screen.AUTH) { AuthenticationScreen(navigationActions) }
+      composable(Screen.CREATE_USER) { UserCreate(navigationActions, userViewModel) }
     }
 
     navigation(
