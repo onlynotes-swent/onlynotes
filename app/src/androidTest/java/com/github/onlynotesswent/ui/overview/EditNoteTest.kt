41ea0af194ccefaffc79d7de95ca789a6365a108
matthieu <saurylericoux@protonmail.com>
2024-11-03 18:13:57 +0100

    Implement the comment system with tests of it

--- app/src/androidTest/java/com/github/onlynotesswent/ui/overview/EditNoteTest.kt
+++ app/src/androidTest/java/com/github/onlynotesswent/ui/overview/EditNoteTest.kt
@@ -61,6 +61,8 @@ class EditNoteTest {
     composeTestRule.onNodeWithTag("goBackButton").assertIsDisplayed()
     composeTestRule.onNodeWithTag("Save button").assertIsDisplayed()
     composeTestRule.onNodeWithTag("Delete button").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("Add Comment Button").assertIsDisplayed()
+
     composeTestRule.onNodeWithTag("visibilityEditButton").assertIsDisplayed()
     composeTestRule.onNodeWithTag("visibilityEditMenu").assertIsNotDisplayed()
 
@@ -74,6 +76,13 @@ class EditNoteTest {
 
     org.mockito.kotlin.verify(navigationActions).goBack()
     org.mockito.kotlin.verify(navigationActions, never()).navigateTo(Screen.OVERVIEW)
+  }
+
+  @Test
+  fun clickAddCommentButton() {
+    composeTestRule.onNodeWithTag("Add Comment Button").performClick()
+    composeTestRule.onNodeWithTag("DeleteCommentButton_0").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("EditCommentTextField_0").assertIsDisplayed()
   }
 
   @Test
