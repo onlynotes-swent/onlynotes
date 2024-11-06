4efefa2c180c6a9df67e158cf5cdae524f8444ef
Adrián Villegas <adrian.villegasherran@epfl.ch>
2024-11-06 18:07:00 +0100

    refactor: Remove add folder screen
    Remove add folder screen which will be replaced by an alert dialog

--- app/src/main/java/com/github/onlynotesswent/MainActivity.kt
+++ app/src/main/java/com/github/onlynotesswent/MainActivity.kt
@@ -20,7 +20,6 @@ import com.github.onlynotesswent.ui.authentication.SignInScreen
 import com.github.onlynotesswent.ui.navigation.NavigationActions
 import com.github.onlynotesswent.ui.navigation.Route
 import com.github.onlynotesswent.ui.navigation.Screen
-import com.github.onlynotesswent.ui.overview.AddFolderScreen
 import com.github.onlynotesswent.ui.overview.AddNoteScreen
 import com.github.onlynotesswent.ui.overview.EditNoteScreen
 import com.github.onlynotesswent.ui.overview.FolderContentScreen
@@ -31,63 +30,65 @@ import com.github.onlynotesswent.ui.user.CreateUserScreen
 import com.github.onlynotesswent.ui.user.ProfileScreen
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    val scanner = Scanner(this).apply { init() }
-    setContent { AppTheme { Surface(modifier = Modifier.fillMaxSize()) { OnlyNotesApp(scanner) } } }
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val scanner = Scanner(this).apply { init() }
+        setContent { AppTheme { Surface(modifier = Modifier.fillMaxSize()) { OnlyNotesApp(scanner) } } }
+    }
 }
 
 @Composable
 fun OnlyNotesApp(scanner: Scanner) {
-  val navController = rememberNavController()
-  val navigationActions = NavigationActions(navController)
-  val userViewModel: UserViewModel = viewModel(factory = UserViewModel.Factory)
-  val noteViewModel: NoteViewModel = viewModel(factory = NoteViewModel.Factory)
-  val folderViewModel: FolderViewModel = viewModel(factory = FolderViewModel.Factory)
+    val navController = rememberNavController()
+    val navigationActions = NavigationActions(navController)
+    val userViewModel: UserViewModel = viewModel(factory = UserViewModel.Factory)
+    val noteViewModel: NoteViewModel = viewModel(factory = NoteViewModel.Factory)
+    val folderViewModel: FolderViewModel = viewModel(factory = FolderViewModel.Factory)
 
-  NavHost(navController = navController, startDestination = Route.AUTH) {
-    navigation(
-        startDestination = Screen.AUTH,
-        route = Route.AUTH,
-    ) {
-      composable(Screen.AUTH) { SignInScreen(navigationActions, userViewModel) }
-      composable(Screen.CREATE_USER) { CreateUserScreen(navigationActions, userViewModel) }
-    }
+    NavHost(navController = navController, startDestination = Route.AUTH) {
+        navigation(
+            startDestination = Screen.AUTH,
+            route = Route.AUTH,
+        ) {
+            composable(Screen.AUTH) { SignInScreen(navigationActions, userViewModel) }
+            composable(Screen.CREATE_USER) { CreateUserScreen(navigationActions, userViewModel) }
+        }
 
-    navigation(
-        startDestination = Screen.OVERVIEW,
-        route = Route.OVERVIEW,
-    ) {
-      composable(Screen.OVERVIEW) {
-        OverviewScreen(navigationActions, noteViewModel, userViewModel, folderViewModel)
-      }
-      composable(Screen.ADD_NOTE) {
-        AddNoteScreen(navigationActions, scanner, noteViewModel, userViewModel)
-      }
-      composable(Screen.EDIT_NOTE) {
-        EditNoteScreen(navigationActions, noteViewModel, userViewModel)
-      }
-      composable(Screen.ADD_FOLDER) {
-        AddFolderScreen(navigationActions, folderViewModel, userViewModel)
-      }
-      composable(Screen.FOLDER_CONTENTS) {
-        FolderContentScreen(navigationActions, folderViewModel, noteViewModel, userViewModel)
-      }
-    }
+        navigation(
+            startDestination = Screen.OVERVIEW,
+            route = Route.OVERVIEW,
+        ) {
+            composable(Screen.OVERVIEW) {
+                OverviewScreen(navigationActions, noteViewModel, userViewModel, folderViewModel)
+            }
+            composable(Screen.ADD_NOTE) {
+                AddNoteScreen(navigationActions, scanner, noteViewModel, userViewModel)
+            }
+            composable(Screen.EDIT_NOTE) {
+                EditNoteScreen(navigationActions, noteViewModel, userViewModel)
+            }
+            composable(Screen.FOLDER_CONTENTS) {
+                FolderContentScreen(
+                    navigationActions,
+                    folderViewModel,
+                    noteViewModel,
+                    userViewModel
+                )
+            }
+        }
 
-    navigation(
-        startDestination = Screen.SEARCH_NOTE,
-        route = Route.SEARCH,
-    ) {
-      composable(Screen.SEARCH_NOTE) { SearchScreen(navigationActions, noteViewModel) }
-    }
+        navigation(
+            startDestination = Screen.SEARCH_NOTE,
+            route = Route.SEARCH,
+        ) {
+            composable(Screen.SEARCH_NOTE) { SearchScreen(navigationActions, noteViewModel) }
+        }
 
-    navigation(
-        startDestination = Screen.PROFILE,
-        route = Route.PROFILE,
-    ) {
-      composable(Screen.PROFILE) { ProfileScreen(navigationActions, userViewModel) }
+        navigation(
+            startDestination = Screen.PROFILE,
+            route = Route.PROFILE,
+        ) {
+            composable(Screen.PROFILE) { ProfileScreen(navigationActions, userViewModel) }
+        }
     }
-  }
 }
--- app/src/main/java/com/github/onlynotesswent/ui/navigation/NavigationActions.kt
+++ app/src/main/java/com/github/onlynotesswent/ui/navigation/NavigationActions.kt
@@ -9,36 +9,36 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 
 object Route {
-  const val OVERVIEW = "Overview"
-  const val AUTH = "Auth"
-  const val SEARCH = "Search"
-  const val PROFILE = "Profile"
+    const val OVERVIEW = "Overview"
+    const val AUTH = "Auth"
+    const val SEARCH = "Search"
+    const val PROFILE = "Profile"
 }
 
 object Screen {
-  const val AUTH = "Auth Screen"
-  const val OVERVIEW = "Overview Screen"
-  const val CREATE_USER = "Create User Screen"
-  const val ADD_NOTE = "Add Note Screen"
-  const val EDIT_NOTE = "Edit Note Screen"
-  const val SEARCH_NOTE = "Search Note Screen"
-  const val PROFILE = "Profile Screen"
-  const val ADD_FOLDER = "Add Folder Screen"
-  const val FOLDER_CONTENTS = "Folder Contents Screen"
+    const val AUTH = "Auth Screen"
+    const val OVERVIEW = "Overview Screen"
+    const val CREATE_USER = "Create User Screen"
+    const val ADD_NOTE = "Add Note Screen"
+    const val EDIT_NOTE = "Edit Note Screen"
+    const val SEARCH_NOTE = "Search Note Screen"
+    const val PROFILE = "Profile Screen"
+    const val FOLDER_CONTENTS = "Folder Contents Screen"
 }
 
 data class TopLevelDestination(val route: String, val icon: ImageVector, val textId: String)
 
 object TopLevelDestinations {
-  // In the overview screen we will display the user notes
-  val OVERVIEW =
-      TopLevelDestination(route = Route.OVERVIEW, icon = Icons.Outlined.Home, textId = "My Notes")
-  val SEARCH =
-      TopLevelDestination(
-          route = Route.SEARCH, icon = Icons.Outlined.Search, textId = "Search Notes")
-  val PROFILE =
-      TopLevelDestination(
-          route = Route.PROFILE, icon = Icons.Outlined.Person, textId = "My Profile")
+    val OVERVIEW =
+        TopLevelDestination(route = Route.OVERVIEW, icon = Icons.Outlined.Home, textId = "My Notes")
+    val SEARCH =
+        TopLevelDestination(
+            route = Route.SEARCH, icon = Icons.Outlined.Search, textId = "Search Notes"
+        )
+    val PROFILE =
+        TopLevelDestination(
+            route = Route.PROFILE, icon = Icons.Outlined.Person, textId = "My Profile"
+        )
 }
 
 val LIST_TOP_LEVEL_DESTINATION =
@@ -47,53 +47,53 @@ val LIST_TOP_LEVEL_DESTINATION =
 open class NavigationActions(
     private val navController: NavHostController,
 ) {
-  /**
-   * Navigate to the specified [TopLevelDestination]
-   *
-   * @param destination The top level destination to navigate to Clear the back stack when
-   *   navigating to a new destination This is useful when navigating to a new screen from the
-   *   bottom navigation bar as we don't want to keep the previous screen in the back stack
-   */
-  open fun navigateTo(destination: TopLevelDestination) {
+    /**
+     * Navigate to the specified [TopLevelDestination]
+     *
+     * @param destination The top level destination to navigate to Clear the back stack when
+     *   navigating to a new destination This is useful when navigating to a new screen from the
+     *   bottom navigation bar as we don't want to keep the previous screen in the back stack
+     */
+    open fun navigateTo(destination: TopLevelDestination) {
 
-    navController.navigate(destination.route) {
-      // Pop up to the start destination of the graph to
-      // avoid building up a large stack of destinations
-      popUpTo(navController.graph.findStartDestination().id) {
-        saveState = true
-        inclusive = true
-      }
+        navController.navigate(destination.route) {
+            // Pop up to the start destination of the graph to
+            // avoid building up a large stack of destinations
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+                inclusive = true
+            }
 
-      // Avoid multiple copies of the same destination when reselecting same item
-      launchSingleTop = true
+            // Avoid multiple copies of the same destination when reselecting same item
+            launchSingleTop = true
 
-      // Restore state when reselecting a previously selected item
-      if (destination.route != Route.AUTH) {
-        restoreState = true
-      }
+            // Restore state when reselecting a previously selected item
+            if (destination.route != Route.AUTH) {
+                restoreState = true
+            }
+        }
     }
-  }
 
-  /**
-   * Navigate to the specified screen.
-   *
-   * @param screen The screen to navigate to
-   */
-  open fun navigateTo(screen: String) {
-    navController.navigate(screen)
-  }
+    /**
+     * Navigate to the specified screen.
+     *
+     * @param screen The screen to navigate to
+     */
+    open fun navigateTo(screen: String) {
+        navController.navigate(screen)
+    }
 
-  /** Navigate back to the previous screen. */
-  open fun goBack() {
-    navController.popBackStack()
-  }
+    /** Navigate back to the previous screen. */
+    open fun goBack() {
+        navController.popBackStack()
+    }
 
-  /**
-   * Get the current route of the navigation controller.
-   *
-   * @return The current route
-   */
-  open fun currentRoute(): String {
-    return navController.currentDestination?.route ?: ""
-  }
+    /**
+     * Get the current route of the navigation controller.
+     *
+     * @return The current route
+     */
+    open fun currentRoute(): String {
+        return navController.currentDestination?.route ?: ""
+    }
 }
