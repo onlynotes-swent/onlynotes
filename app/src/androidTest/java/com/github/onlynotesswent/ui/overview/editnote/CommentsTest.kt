930b821d4f5d0c8905884db6330c2374f12c8aa6
Roshan-y <115220107+Roshan-y@users.noreply.github.com>
2024-11-22 00:09:10 +0100

    test: add comments test

---
+++ app/src/androidTest/java/com/github/onlynotesswent/ui/overview/editnote/CommentsTest.kt
@@ -0,0 +1,33 @@
+package com.github.onlynotesswent.ui.overview.editnote
+
+class CommentsTest {
+  /* @Test
+  fun addCommentAndUpdateNote() {
+      composeTestRule.onNodeWithTag("Add Comment Button").performScrollTo().performClick()
+      composeTestRule.onNodeWithTag("EditCommentTextField").performScrollTo().assertIsDisplayed()
+      val updatedCommentText = "Edited comment content"
+      composeTestRule.onNodeWithTag("EditCommentTextField").performTextInput(updatedCommentText)
+      val expectedLabelText = "edited: "
+      composeTestRule.onNode(hasText(expectedLabelText, substring = true)).assertIsDisplayed()
+      composeTestRule.onNodeWithTag("Save button").performScrollTo().performClick()
+  }
+
+  @Test
+  fun addComment() {
+      composeTestRule.onNodeWithTag("Add Comment Button").performScrollTo().performClick()
+      composeTestRule.onNodeWithTag("EditCommentTextField").performScrollTo().assertIsDisplayed()
+      val updatedCommentText = "Edited comment content"
+      composeTestRule.onNodeWithTag("EditCommentTextField").performTextInput(updatedCommentText)
+      val expectedLabelText = "edited: "
+      composeTestRule.onNode(hasText(expectedLabelText, substring = true)).assertIsDisplayed()
+  }
+
+  @Test
+  fun addCommentAndDeleteComment() {
+      composeTestRule.onNodeWithTag("Add Comment Button").performScrollTo().assertIsDisplayed()
+      composeTestRule.onNodeWithTag("Add Comment Button").performClick()
+      composeTestRule.onNodeWithTag("EditCommentTextField").performScrollTo().assertIsDisplayed()
+      composeTestRule.onNodeWithTag("DeleteCommentButton").performScrollTo().performClick()
+      composeTestRule.onNodeWithTag("Save button").performScrollTo().assertIsDisplayed()
+  }*/
+}
