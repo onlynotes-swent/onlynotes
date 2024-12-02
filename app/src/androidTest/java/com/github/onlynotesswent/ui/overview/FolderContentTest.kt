8c2f2c5f7641a8fa8225c496a63181677e7aef33
Aisel Abibula <abibula.aisel@gmail.com>
2024-12-02 16:30:31 +0100

    style: run ktfmt

--- app/src/androidTest/java/com/github/onlynotesswent/ui/overview/FolderContentTest.kt
+++ app/src/androidTest/java/com/github/onlynotesswent/ui/overview/FolderContentTest.kt
@@ -30,12 +30,10 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
-import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.any
 import org.mockito.kotlin.eq
 
