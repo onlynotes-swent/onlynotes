93ead16ab48a07a2e93584cc39869d2372dbbe03
Adrián Villegas <adrian.villegasherran@epfl.ch>
2024-12-05 17:14:37 +0100

    style: run ktfmt format

--- app/src/test/java/com/github/onlynotesswent/ui/navigation/NavigationActionsTest.kt
+++ app/src/test/java/com/github/onlynotesswent/ui/navigation/NavigationActionsTest.kt
@@ -3,27 +3,77 @@ package com.github.onlynotesswent.ui.navigation
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptionsBuilder
+import com.github.onlynotesswent.model.folder.Folder
+import com.github.onlynotesswent.model.notification.NotificationRepositoryFirestore
+import com.github.onlynotesswent.model.user.Friends
+import com.github.onlynotesswent.model.user.User
+import com.github.onlynotesswent.model.user.UserRepositoryFirestore
+import com.github.onlynotesswent.model.user.UserViewModel
+import com.google.firebase.FirebaseApp
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.any
 import org.mockito.kotlin.eq
+import org.robolectric.RobolectricTestRunner
 
+@Suppress("UNCHECKED_CAST")
+@RunWith(RobolectricTestRunner::class)
 class NavigationActionsTest {
+
+  @Mock private lateinit var mockRepositoryFirestore: UserRepositoryFirestore
+  @Mock private lateinit var mockRepostioryFirestoreNotification: NotificationRepositoryFirestore
+  private lateinit var userViewModel: UserViewModel
 
   @Mock private lateinit var mockNavigationDestination: NavDestination
   @Mock private lateinit var mockNavHostController: NavHostController
   private lateinit var navigationActions: NavigationActions
 
+  private val folder = Folder("1", "folderName", "1")
+  private val subfolder = Folder("2", "subfolderName", "1", "1")
+
+  private val user =
+      User(
+          firstName = "User",
+          lastName = "Name",
+          userName = "username",
+          email = "example@gmail.com",
+          uid = "1",
+          dateOfJoining = Timestamp.now(),
+          rating = 0.0,
+          friends = Friends(following = listOf("3"), followers = listOf("3")))
+
   @Before
   fun setUp() {
     MockitoAnnotations.openMocks(this)
     navigationActions = NavigationActions(mockNavHostController)
+
+    val mockFirebaseAuth = mock(FirebaseAuth::class.java)
+    val mockFirebaseUser = mock(FirebaseUser::class.java)
+    `when`(mockFirebaseAuth.currentUser).thenReturn(mockFirebaseUser)
+
+    // Mock the add user method to call onSuccess
+    `when`(mockRepositoryFirestore.addUser(any(), any(), any())).thenAnswer {
+      val onSuccess = it.arguments[1] as () -> Unit
+      onSuccess()
+    }
+
+    // Initialize FirebaseApp with Robolectric context
+    val context = org.robolectric.RuntimeEnvironment.getApplication()
+    FirebaseApp.initializeApp(context)
+
+    // Initialize UserViewModel with the mocked repository and FirebaseAuth
+    userViewModel = UserViewModel(mockRepositoryFirestore, mockRepostioryFirestoreNotification)
   }
 
   @Test
@@ -50,5 +100,28 @@ class NavigationActionsTest {
     `when`(mockNavigationDestination.route).thenReturn(Route.OVERVIEW)
 
     assertThat(navigationActions.currentRoute(), `is`(Route.OVERVIEW))
+  }
+
+  @Test
+  fun getPreviousScreenCallsPreviousBackStackEntry() {
+    navigationActions.getPreviousScreen()
+    verify(mockNavHostController).previousBackStackEntry
+  }
+
+  @Test
+  fun goBackFolderContentsCallsNavigateTo() {
+    `when`(mockRepositoryFirestore.addUser(any(), any(), any())).thenAnswer {
+      val onSuccess = it.arguments[1] as () -> Unit
+      onSuccess()
+    }
+    userViewModel.addUser(user, {}, {})
+    navigationActions.goBackFolderContents(subfolder, userViewModel)
+    verify(mockNavHostController)
+        .navigate(
+            Screen.FOLDER_CONTENTS.replace(
+                oldValue = "{folderId}", newValue = subfolder.parentFolderId!!))
+
+    navigationActions.goBackFolderContents(folder, userViewModel)
+    verify(mockNavHostController).navigate(eq(Route.OVERVIEW), any<NavOptionsBuilder.() -> Unit>())
   }
 }
