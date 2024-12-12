958ae20a88fdb85e86e0b2eb12055a8e52c37c26
Roshan-y <115220107+Roshan-y@users.noreply.github.com>
2024-12-12 22:53:50 +0100

    refactor: remove debug print statements from isInternetAvailable function

--- app/src/main/java/com/github/onlynotesswent/utils/NetworkUtils.kt
+++ app/src/main/java/com/github/onlynotesswent/utils/NetworkUtils.kt
@@ -14,17 +14,8 @@ object NetworkUtils {
   fun isInternetAvailable(context: Context): Boolean {
     val connectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    println("connectivityManager: $connectivityManager")
     val network = connectivityManager.activeNetwork ?: return false
-    println("network: $network")
     val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
-    println("capabilities: $capabilities")
-    println(
-        "capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET): " +
-            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET))
-    println(
-        "capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED): " +
-            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED))
     return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
         capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
   }
