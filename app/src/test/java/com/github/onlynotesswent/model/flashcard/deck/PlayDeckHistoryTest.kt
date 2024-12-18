df9c737a281536b7d28fa74a5d20884916260f4d
Lawrence Leitgib <lawrence11.leitgib@icloud.com>
2024-12-18 20:08:49 +0100

    test: adapt test from the new methodes

--- app/src/test/java/com/github/onlynotesswent/model/flashcard/deck/PlayDeckHistoryTest.kt
+++ app/src/test/java/com/github/onlynotesswent/model/flashcard/deck/PlayDeckHistoryTest.kt
@@ -7,13 +7,13 @@ class PlayDeckHistoryTest {
   @Test
   fun `test goForwardWithNewFlashcard`() {
     val playDeckHistory = PlayDeckHistory(currentFlashcardId = "1")
-    val newPlayDeckHistory = playDeckHistory.goForwardWithNewFlashcard("2")
+    val newPlayDeckHistory = playDeckHistory.stayWithNewFlashcard("2")
     assert(newPlayDeckHistory.listOfAllFlashcard[1] == "1")
     assert(newPlayDeckHistory.listOfAllFlashcard[2] == "2")
     assert(newPlayDeckHistory.listOfAllFlashcard[3] == null)
     assert(newPlayDeckHistory.listOfAllFlashcard[0] == null)
-    assert(newPlayDeckHistory.currentFlashcardId == "2")
-    assert(newPlayDeckHistory.indexOfCurrentFlashcard == 2)
+    assert(newPlayDeckHistory.currentFlashcardId == "1")
+    assert(newPlayDeckHistory.indexOfCurrentFlashcard == 1)
     assert(newPlayDeckHistory.size == 2)
   }
 
@@ -23,20 +23,26 @@ class PlayDeckHistoryTest {
     assert(playDeckHistory.size == 1)
     assert(!playDeckHistory.canGoBack())
 
-    val newPlayDeckHistory = playDeckHistory.goForwardWithNewFlashcard("2")
+    val newPlayDeckHistory = playDeckHistory.stayWithNewFlashcard("2")
     assert(newPlayDeckHistory.size == 2)
-    assert(newPlayDeckHistory.canGoBack())
-    assert(!newPlayDeckHistory.canGoForward())
-    assert(newPlayDeckHistory.currentFlashcardId == "2")
+    assert(newPlayDeckHistory.currentFlashcardId == "1")
 
-    val newPlayDeckHistory2 = newPlayDeckHistory.goBack()
-    assert(newPlayDeckHistory2.size == 2)
-    assert(!newPlayDeckHistory2.canGoBack())
+    val newPlayDeckHistory2 = newPlayDeckHistory.goForwardWithTwiceNextFlashcard("3")
+    assert(newPlayDeckHistory2.size == 3)
+    assert(newPlayDeckHistory2.currentFlashcardId == "2")
+    assert(newPlayDeckHistory2.canGoBack())
     assert(newPlayDeckHistory2.canGoForward())
+    assert(!newPlayDeckHistory2.canGoTwiceForward())
 
-    val newPlayDeckHistory3 = newPlayDeckHistory2.goForward()
-    assert(newPlayDeckHistory3.size == 2)
-    assert(newPlayDeckHistory3.canGoBack())
-    assert(!newPlayDeckHistory3.canGoForward())
+    val newPlayDeckHistory3 = newPlayDeckHistory2.goBack()
+    assert(newPlayDeckHistory3.size == 3)
+    assert(!newPlayDeckHistory3.canGoBack())
+    assert(newPlayDeckHistory3.canGoForward())
+
+    val newPlayDeckHistory4 = newPlayDeckHistory3.goForward()
+    assert(newPlayDeckHistory4.size == 3)
+    assert(newPlayDeckHistory4.canGoBack())
+    assert(newPlayDeckHistory4.canGoForward())
+    assert(!newPlayDeckHistory4.canGoTwiceForward())
   }
 }
