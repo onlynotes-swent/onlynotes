17feef097871e185368dd74a76bcea4526060b3a
Roshan-y <115220107+Roshan-y@users.noreply.github.com>
2024-12-19 09:28:06 +0100

    feat: update comment

--- app/src/main/java/com/github/onlynotesswent/ui/overview/editnote/Comments.kt
+++ app/src/main/java/com/github/onlynotesswent/ui/overview/editnote/Comments.kt
@@ -1,23 +1,19 @@
 package com.github.onlynotesswent.ui.overview.editnote
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.FloatingActionButton
@@ -28,6 +24,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -37,25 +34,28 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.github.onlynotesswent.R
+import com.github.onlynotesswent.model.file.FileViewModel
 import com.github.onlynotesswent.model.note.Note
 import com.github.onlynotesswent.model.note.NoteViewModel
 import com.github.onlynotesswent.model.user.User
 import com.github.onlynotesswent.model.user.UserViewModel
+import com.github.onlynotesswent.ui.common.ThumbnailDynamicPic
 import com.github.onlynotesswent.ui.navigation.NavigationActions
 import com.github.onlynotesswent.ui.navigation.Screen
-import java.text.SimpleDateFormat
-import java.util.Locale
+import com.github.onlynotesswent.ui.theme.Typography
 
 @Composable
 fun CommentsScreen(
     navigationActions: NavigationActions,
     noteViewModel: NoteViewModel,
-    userViewModel: UserViewModel
+    userViewModel: UserViewModel,
+    fileViewModel: FileViewModel
 ) {
   val note by noteViewModel.selectedNote.collectAsState()
   val currentUser by userViewModel.currentUser.collectAsState()
@@ -122,7 +122,8 @@ fun CommentsScreen(
                       { updatedComments = it },
                       currentUser!!,
                       note!!,
-                      userViewModel)
+                      userViewModel,
+                      fileViewModel)
                 }
 
             // SendCommentBar fixed at the bottom
@@ -203,7 +204,8 @@ fun CommentsSection(
     onCommentsChange: (Note.CommentCollection) -> Unit,
     currentUser: User,
     note: Note,
-    userViewModel: UserViewModel
+    userViewModel: UserViewModel,
+    fileViewModel: FileViewModel
 ) {
   if (updatedComments.commentsList.isEmpty()) {
     NoCommentsText()
@@ -215,7 +217,8 @@ fun CommentsSection(
           onCommentsChange = onCommentsChange,
           currentUser = currentUser,
           note = note,
-          userViewModel)
+          userViewModel,
+          fileViewModel)
     }
   }
 }
@@ -228,7 +231,6 @@ fun NoCommentsText() {
       modifier = Modifier.padding(8.dp).testTag("noCommentsText"))
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CommentRow(
     comment: Note.Comment,
@@ -236,95 +238,90 @@ fun CommentRow(
     onCommentsChange: (Note.CommentCollection) -> Unit,
     currentUser: User,
     note: Note,
-    userViewModel: UserViewModel
+    userViewModel: UserViewModel,
+    fileViewModel: FileViewModel
 ) {
   var isEditing by remember { mutableStateOf(false) }
   var currentContent by remember { mutableStateOf(comment.content) }
   var expanded by remember { mutableStateOf(false) } // Shared state for CommentOptionsMenu
-  Row(
-      modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
-      verticalAlignment = Alignment.CenterVertically,
-      horizontalArrangement = Arrangement.Start) {
-        if (isEditing) {
-          Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp).padding(8.dp)) {
-            OutlinedTextField(
-                value = currentContent,
-                onValueChange = { currentContent = it },
-                label = {
-                  val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
-                  val formattedDate = formatter.format(comment.editedDate.toDate())
-                  val displayedText =
-                      if (comment.isUnedited()) {
-                        "${comment.userName} : $formattedDate"
-                      } else {
-                        stringResource(R.string.edited, comment.userName, formattedDate)
-                      }
-                  Text(displayedText)
-                },
-                placeholder = { Text(stringResource(R.string.enter_comment_here)) },
-                modifier = Modifier.weight(1f).testTag("EditCommentTextField"))
-            IconButton(
-                onClick = {
-                  val updatedCollection =
-                      updatedComments.editCommentByCommentId(comment.commentId, currentContent)
-                  onCommentsChange(updatedCollection)
-                  isEditing = false
-                }) {
-                  Icon(imageVector = Icons.Default.Check, contentDescription = "Save Comment")
-                }
-          }
-        } else {
-          Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)) {
-            Text(
-                text = comment.userName, // Replace with your desired title
-                style =
-                    MaterialTheme.typography.bodySmall.copy(
-                        color = MaterialTheme.colorScheme.onBackground),
-                modifier = Modifier.padding(bottom = 4.dp) // Space between title and row
-                )
-            // Outlined Row
-            Row(
-                modifier =
-                    Modifier.fillMaxWidth()
-                        .border(
-                            1.dp,
-                            MaterialTheme.colorScheme.primary,
-                            shape = RoundedCornerShape(4.dp))
-                        .padding(8.dp)
-                        .combinedClickable(
-                            onClick = { /* Add desired click behavior if needed */},
-                            onLongClick = {
-                              if (comment.isOwner(currentUser.uid) ||
-                                  note.isOwner(currentUser.uid)) {
-                                expanded = true
-                              }
-                            })) {
-                  Text(
-                      text = comment.content,
-                      modifier =
-                          Modifier.weight(
-                                  1f) // Ensures the text takes up all available space before the
-                              // Spacer
-                              .padding(
-                                  end = 8.dp) // Optional: adds some space between text and options
-                      // menu
-                      )
-                  Spacer(modifier = Modifier.weight(0.1f)) // Add this to push the menu to the end
-                  if (comment.isOwner(currentUser.uid) || note.isOwner(currentUser.uid)) {
-                    CommentOptionsMenu(
-                        comment = comment,
-                        updatedComments = updatedComments,
-                        onCommentsChange = onCommentsChange,
-                        currentUser = currentUser,
-                        note = note,
-                        expanded = expanded,
-                        onDismissRequest = { expanded = false },
-                        onEditRequest = { isEditing = true })
-                  }
-                }
-          }
+  val commentUser = remember { mutableStateOf<User?>(null) }
+  userViewModel.getUserById(comment.userId, { commentUser.value = it })
+  if (commentUser.value == null) {
+    return
+  }
+  if (isEditing) {
+    // Editing Mode
+    // Top Row: Thumbnail, User Handle, and Options Menu
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp, vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+          // Thumbnail on the left
+          ThumbnailDynamicPic(commentUser, fileViewModel, size = 30)
+
+          // User handle in the center
+          Text(
+              text = commentUser.value!!.userHandle(),
+              style = Typography.bodyMedium,
+              modifier = Modifier.weight(2f).alpha(0.7f).padding(start = 4.dp))
         }
-      }
+    Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp, vertical = 4.dp)) {
+      TextField(
+          value = currentContent,
+          onValueChange = { currentContent = it },
+          placeholder = { Text(stringResource(R.string.enter_comment_here)) },
+          modifier = Modifier.weight(1f).testTag("EditCommentTextField"))
+      IconButton(
+          onClick = {
+            val updatedCollection =
+                updatedComments.editCommentByCommentId(comment.commentId, currentContent)
+            onCommentsChange(updatedCollection)
+            isEditing = false
+          }) {
+            Icon(imageVector = Icons.Default.Check, contentDescription = "Save Comment")
+          }
+    }
+  } else {
+    Column(modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)) {
+      // Top Row: Thumbnail, User Handle, and Options Menu
+      Row(
+          modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp, vertical = 4.dp),
+          verticalAlignment = Alignment.CenterVertically) {
+            // Thumbnail on the left
+            ThumbnailDynamicPic(commentUser, fileViewModel, size = 30)
+
+            // User handle in the center
+            Text(
+                text = commentUser.value!!.userHandle(),
+                style = Typography.bodyMedium,
+                modifier = Modifier.weight(2f).alpha(0.7f).padding(start = 4.dp))
+            // Options menu on the right
+            if (comment.isOwner(currentUser.uid) || note.isOwner(currentUser.uid)) {
+              Box { // Anchor box for the dropdown
+                IconButton(onClick = { expanded = true }) {
+                  Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More Options")
+                }
+                CommentOptionsMenu(
+                    comment = comment,
+                    updatedComments = updatedComments,
+                    onCommentsChange = onCommentsChange,
+                    currentUser = currentUser,
+                    note = note,
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    onEditRequest = { isEditing = true })
+              }
+            }
+          }
+
+      // Second Row: Full-width comment content
+      Text(
+          text = comment.content,
+          modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp),
+          style = Typography.bodyLarge)
+    }
+  }
+  // add horizontal divider between comments
+  HorizontalDivider(color = Color.Gray, thickness = 0.5.dp)
 }
 
 @Composable
