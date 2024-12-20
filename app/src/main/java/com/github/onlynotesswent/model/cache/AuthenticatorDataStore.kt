a34bc3d141677b39b42121190e1d94407b31122c
Aisel Abibula <abibula.aisel@gmail.com>
2024-12-20 07:41:02 +0100

    docs: add missing documentation

--- app/src/main/java/com/github/onlynotesswent/model/cache/AuthenticatorDataStore.kt
+++ app/src/main/java/com/github/onlynotesswent/model/cache/AuthenticatorDataStore.kt
@@ -11,6 +11,11 @@ import kotlinx.coroutines.flow.map
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "auth")
 
+/**
+ * Manager for the authentication state of the user. Implemented using DataStore Preferences.
+ *
+ * @param context The application context
+ */
 class AuthManager(private val context: Context) {
   companion object {
     private val USER_EMAIL = stringPreferencesKey("user_email")
@@ -20,11 +25,13 @@ class AuthManager(private val context: Context) {
   val userEmailFlow: Flow<String?> =
       context.dataStore.data.map { preferences -> preferences[USER_EMAIL] }
 
-  /** Save the user's email in the data store */
+  /**
+   * Save the user's email in the data store
+   *
+   * @param userEmail The email to save
+   */
   suspend fun saveCredentials(userEmail: String) {
-    context.dataStore.edit { preferences ->
-      preferences[USER_EMAIL] = userEmail
-    }
+    context.dataStore.edit { preferences -> preferences[USER_EMAIL] = userEmail }
   }
 
   /** Delete saved credentials from the data store */
